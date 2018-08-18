97d440dfb31e0c19f06052408b2e3fd810aa3832
ning.liu <ning.liu@linkplay.com>
2018-08-18 15:18:40 +0800

    more practices

---
+++ cplusplus/1.cpp
@@ -0,0 +1,17 @@
+class Point
+{
+public:
+  void setPoint(int x, int y)
+  {
+    xPos = x;
+    yPos = y;
+  }
+
+  void printPoint()
+  {
+  }
+
+private:
+  int xPos;
+  int yPos;
+};
---
+++ cpractice/debug.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argv, char *argc[]) {
+  char *a = NULL;
+  char *b = calloc(1, sizeof(char) * 10);
+  return 0;
+}
